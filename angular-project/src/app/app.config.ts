f5c44aa998feaa05e89df821ea3333bec06ec426
victsilva37 <victormau.sm@gmail.com>
2025-08-12 13:27:06 -0400

    Configuración del componente raíz para utliuzar los componentes hijos

--- angular-project/src/app/app.config.ts
+++ angular-project/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(HttpClientModule),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
